417a4136d4fb970357e6901c697bcf5fd2d15b06
harrison rex <harrisonrex03@gmail.com>
2023-11-22 18:36:03 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] argms) {
+
+        String name = "harrison chege";
+        String country="Kenya";
+        age = 23;
+        String company="freecodecamp";
+        System.out.printl("my name is "+name+" i am from "+country+" I am "+age+" years old,the company i work is called 
+        "+company+"
+        ");
+    }
+
+}
---
+++ java.js
@@ -0,0 +1,6 @@
+ 
+    function submitForm() {
+      var comment = document.getElementById("comment").value;
+      alert("Submitted Comment: " + comment);
+    }
+  
